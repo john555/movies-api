1cbf83d22fedb4f916bee2da3abd07278319c575
John O. Paul <oyomthojohnpaul@gmail.com>
2018-09-12 09:09:12 +0300

    (feature): Add ability to create user and login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const secret = process.env.SECRET;
 const router = express.Router();
@@ -14,19 +15,51 @@ router.post('/', (request, response) => {
     password
   } = request.body;
 
-  const user = new User({ 
-    firstName, 
-    lastName, 
-    username, 
-    passwordHash: password
-  });
+  User.fin
 
-  user.save((err, user) => {
-    if(err) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
       return response.status(400).send(error);
     }
-    return response.status(201).send(user);
-  })
+
+    bcrypt.hash(password, salt, (err, hash) => {
+      if (err) {
+        return response.status(400).send(error);
+      }
+
+      User.findOne({ username }).exec((err, user) => {
+        if(err) {
+          return response.status(400).send(error);
+        }
+
+        if(user) {
+          error.message = 'Username already exists';
+          return response.status(400).send(error);
+        }
+
+        const newUser = new User({ 
+          firstName, 
+          lastName, 
+          username, 
+          passwordHash: hash
+        });
+  
+        newUser.save((err, user) => {
+          if(err) {
+            return response.status(400).send(error);
+          }
+  
+          return response.status(201).send({
+            id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username
+          });
+        });
+      });
+    });
+  });
+  
 });
 
 router.post('/login', (request, response) => {
@@ -37,16 +70,28 @@ router.post('/login', (request, response) => {
       return response.status(400).send(error);
     }
 
-    jwt.sign({ user }, secret, (err, token) => {
-      if (err) {
-        return response.status(400).send(error);
+    bcrypt.compare(password, user.passwordHash, (err, result) => {
+      if (!result) {
+        error.message = 'Invalid login credentials';
+        return response.status(401).send(error);
       }
-      
-      return response.status(201).send({
-        user,
-        token
-      });
-    })
+
+      jwt.sign({ user }, secret, (err, token) => {
+        if (err) {
+          return response.status(400).send(error);
+        }
+  
+        return response.status(201).send({
+          user: {
+            id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username
+          },
+          token,
+        });
+      })
+    });
   });
 })
 
